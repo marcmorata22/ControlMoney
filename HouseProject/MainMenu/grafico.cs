0d5e7f7711c007cda0dbc5472a65d7ebec5e36e5
Marc <43479593+MORATA22@users.noreply.github.com>
2019-10-17 18:47:34 +0200

    char easy

--- HouseProject/MainMenu/grafico.cs
+++ HouseProject/MainMenu/grafico.cs
@@ -10,25 +10,23 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
-
 namespace MainMenu
 {
     public partial class grafico : Form
     {
         #region Global Vars   
         ConectionClass.DBConnectionClass connectBBDD = new ConectionClass.DBConnectionClass();
+        string[] listMonth = { "enero", "febrero", "marzo", "abril","mayo","junio","julio","agosto",
+        "septiembre","octubre","noviembre","diciembre"};        
         DataSet dts;
         DataTable data;
         SqlCommand command = new SqlCommand();
-        Series x = new Series();
         Series y = new Series();
-        DateTime[] setsdate;
-        DateTime date;
-        public string query = "select * from graficoMoney";
-        double[] setstotal;        
+        string query = "select * from graficoMoney";
+        string month = "";
         double movement = 0;
-        double total = 0;
-        int i;      
+        double total = 0;        
+        int i;
         #endregion Global Vars
 
         #region Builder
@@ -41,52 +39,57 @@ namespace MainMenu
         #region Methods
         public void grafico_Load(object sender, EventArgs e)
         {
-            dts = connectBBDD.graphdata();
-            data = dts.Tables[0];
-            fillChar();
+            try
+            {
+                dts = connectBBDD.graphdata();
+                data = dts.Tables[0];
+                fillChar();
+            }
+            catch(Exception Ge)
+            {
+                MessageBox.Show(Ge.Message);
+            }
         }
+        private string fillChar()
+        {
+            string _fillChar = "";           
+            foreach (DataRow row in data.Rows)
+            {
+                total = Convert.ToDouble(row["total"]);
+                month = Convert.ToString(row["month"]);                
+                if (listMonth.Contains(month))
+                {
+                    y = grafMoney.Series.Add(month.ToString());
+                    y.Points.Add(total);
+                }               
+            }
+            return _fillChar;
+        }        
         private void insertData()
         {
+            foreach (DataRow row in data.Rows)
+            {
+                movement = Convert.ToDouble(txtmovement.Text);
+                total = Convert.ToDouble(row["total"]);
+            }
             DataRow rows = data.NewRow();                  
-            rows["total"] = calculGraph();
+            rows["total"] = calculGraph(total,movement);
             rows["date"] = txtdate.Text;
             rows["description"] = txtdescrip.Text;
             rows["bankingmovement"] = txtmovement.Text;
             data.Rows.Add(rows);
             connectBBDD.update(dts, query);
         }     
-        private double calculGraph()
+        private double calculGraph(double total, double movement)
         {
-            double _calculGraph = 0;                        
-            foreach (DataRow row in data.Rows)
-            {
-                movement = Convert.ToDouble(txtmovement.Text);
-                _calculGraph = Convert.ToDouble(row["total"]);
-                for (i = 0; i < 1; i++)
+            double _calculGraph = total;
+
+            for (i = 0; i < 1; i++)
                 {
                     _calculGraph += movement;                   
-                }            
-            }            
+                }                                    
             return _calculGraph;
-        }
-        private string fillChar()
-        {
-            string _fillChar = "";
-            foreach (DataRow row in data.Rows)
-            {
-                total = Convert.ToDouble(row["total"]);
-                date = Convert.ToDateTime(row["date"]);
-                //Series x = grafMoney.Series.Add(total.ToString());
-                Series y = grafMoney.Series.Add(date.ToString());
-                y.Label = total.ToString();
-                y.Points.Add(total);
-               
-            }
-
-            
-
-            return _fillChar;
-        }
+        }        
         #endregion Methods     
 
         #region Events
@@ -97,6 +100,3 @@ namespace MainMenu
         #endregion Events
     }
 }
- //coger mes de la cadena fecha
- //enum para los meses
- //ir sumando según el mes
